92bcc806ddde297b2acc073c934655681c35363b
sukritchandra <80507423+sukritchandra@users.noreply.github.com>
2021-04-26 01:54:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ $(".submit-btn").mouseenter(function() {
 $(".submit-btn").on("click", function(){
 
 var a = $("#myInput").val();
+var b = a.fontcolor("red");
 var d = new Date();
 var e = d.getMonth()+1;
 
 
 
-$(".formdata").html("You have chosen "   + a +  " as Top-Gainer on " + d.getDate() +"/" + e + "/" + d.getFullYear());
+$(".formdata").html("<span style = 'font-size: 2rem'> You have chosen </span>"
++ b +  " <span style = 'font-size: 2rem'> as Top-Gainer on </span> " + d.getDate() +"/" + e + "/" + d.getFullYear());
 
 $(".formdata").addClass("lastMessage");
 
 });
+
