cec5702ea68edcfa942d044c66df77aa6377c004
sukritchandra <80507423+sukritchandra@users.noreply.github.com>
2021-04-28 15:58:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+
 $(".submit-btn").mouseenter(function() {
   $( ".bull" ).fadeOut( 100 );
   $( ".bull" ).fadeIn( 100 );
 });
+
+
+var d = new Date();
+var e = d.getMonth()+1;
 
 
 
@@ -9,15 +14,19 @@ $(".submit-btn").on("click", function(){
 
 var a = $("#myInput").val();
 var b = a.fontcolor("red");
-var d = new Date();
-var e = d.getMonth()+1;
+var c = $("#email").val();
+var regEx =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+if (a === "" || c === "" ){alert ("Enter All Inputs"); return false;
+}
 
 
 
-$(".formdata").html("<span style = 'font-size: 2rem'> You have chosen </span>"
+else if(c.match( regEx))  { $(".formdata").html("<span style = 'font-size: 2rem'> You have chosen </span>"
 + b +  " <span style = 'font-size: 2rem'> as Top-Gainer on </span> " + d.getDate() +"/" + e + "/" + d.getFullYear());
-
 $(".formdata").addClass("lastMessage");
+}
+
+else {alert ("Enter Valid Email ID"); return false; }
 
 });
-
